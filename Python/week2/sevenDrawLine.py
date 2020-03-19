dfbcdaf2f5a88e529a4d9044609d4d2a0e8aa20e
ggz_66@163.com <ggz_66@163.com>
2020-03-19 21:31:52 +0800

    七段数码管显示时间

--- Python/week2/sevenDrawLine.py
+++ Python/week2/sevenDrawLine.py
@@ -1,10 +1,15 @@
 #七段数码管
-import turtle
+import turtle,time
+def drawGap():#绘制数码管间隔
+    turtle.penup()
+    turtle.fd(5)
 def drawLine(draw):#绘制单段数码管
+    drawGap()
     turtle.pendown() if draw else turtle.penup()
     turtle.fd(40)
+    drawGap()
     turtle.right(90)
-def drawDigit(digit):
+def drawDigit(digit):#绘制多段数码管
     drawLine(True) if digit in [2,3,4,5,6,8,9] else drawLine(False)
     drawLine(True) if digit in [0,1,3,4,5,6,8,9] else drawLine(False)
     drawLine(True) if digit in [0,2,3,5,6,8,9] else drawLine(False)
@@ -17,14 +22,27 @@ def drawDigit(digit):
     turtle.penup()#为绘制后续数字确定位置
     turtle.fd(20)#为绘制后需数值确定位置
 def drawDate(date):#获得要输出的数字
+    turtle.pencolor("red")
     for i in date:
-        drawDigit(eval(i))#通过eval()函数将数值变为整数
+        if i=='-':
+            turtle.write('年',font=("Arial",18,"normal"))
+            turtle.pencolor("green")
+            turtle.fd(40)
+        elif i=='=':
+            turtle.write('月',font=("Arial",18,"normal"))
+            turtle.pencolor("blue")
+            turtle.fd(40)
+        elif i=='+':
+            turtle.write('日',font=("Arial",18,"normal"))
+        else:
+            drawDigit(eval(i))#通过eval()函数将数值变为整数
 def main():
     turtle.setup(800,350,200,200)
     turtle.penup()
-    turtle.fd(-300)
+    turtle.fd(-350)
     turtle.pensize(5)
-    drawDate('20181010')
+    #drawDate('20200312')
+    drawDate(time.strftime('%Y-%m=%d+',time.gmtime()))
     turtle.hideturtle()
     turtle.done()
 main()
