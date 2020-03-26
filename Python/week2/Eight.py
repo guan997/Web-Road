1993fbbf33dfdd35ea7ced810066254cbacf2708
ggz_66@163.com <ggz_66@163.com>
2020-03-26 21:40:30 +0800

    列表

---
+++ Python/week2/Eight.py
@@ -0,0 +1,13 @@
+#Eight
+import turtle as t
+'''t.pensize(2)
+t.setup(600,600,200,100)
+for i in range(8):
+     t.fd(100)
+     t.right(45)'''
+
+t.pensize(2)
+t.setup(600,600,200,100)
+for i in range(8):
+     t.fd(150)
+     t.left(135)
