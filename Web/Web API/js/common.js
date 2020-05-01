3e8ddd084ca167d79b528170936697c16d9561eb
ggz_66@163.com <ggz_66@163.com>
2020-05-01 23:02:58 +0800

    获取盒子在页面上的位置

--- Web/Web API/js/common.js
+++ Web/Web API/js/common.js
@@ -72,3 +72,23 @@ function removeEventListener(element, eventName, fn) {
         element['on' + eventName] = null;
     }
 }
+// 获取页面滚动距离的浏览器兼容性问题
+// 获取页面滚动出去的距离
+function getScroll() {
+    var scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    return {
+        scrollLeft: scrollLeft,
+        scrollTop: scrollTop
+    }
+
+}
+// 获取鼠标在页面的位置，处理浏览器兼容性
+function getPage(e) {
+    var pageX = e.pageX || e.clientX + getScroll().scrollLeft;
+    var pageY = e.pageY || e.clientY + getScroll().scrollTop;
+    return {
+        pageX: pageX,
+        pageY: pageY
+    }
+}
