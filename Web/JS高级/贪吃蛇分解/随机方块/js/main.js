13293e5af08a8c30216ed98f6437fdf9fb03fbb8
ggz_66@163.com <ggz_66@163.com>
2020-05-10 15:24:55 +0800

    更新

--- Web/JS高级/贪吃蛇分解/随机方块/js/main.js
+++ Web/JS高级/贪吃蛇分解/随机方块/js/main.js
@@ -1,7 +1,9 @@
 ;
 (function (window, document, unit, Food, Snake) {
+    // 创建数组
     var foods = [];
     for (var i = 0; i < 10; i++) {
+        // 把创建好的方块对象添加到数组中
         foods.push(new Food({
             color: `rgb(${util.getRandom(0, 256)},
             ${util.getRandom(0, 256)},${util.getRandom(0, 256)})`
