f161feba2f2fa6acf53ad433d05d490e669ff71b
ggz_66@163.com <ggz_66@163.com>
2020-05-11 23:59:38 +0800

    04-贪吃蛇-move方法

---
+++ Web/JS高级/贪吃蛇分解/04-贪吃蛇-move方法/js/game.js
@@ -0,0 +1,25 @@
+// 使用自调用函数，创建一个新的局部作用域，防止命名冲突
+(function () {
+    function Game(map) {
+        this.food = new Food();
+        this.snake = new Snake();
+        this.map = map;
+    } 
+    Game.prototype.start = function () {
+        // 1.把蛇和食物对象，渲染到地图上
+        this.food.render(this.map);
+        this.snake.move(this.map);
+        this.snake.render(this.map);
+        this.snake.move(this.map);
+        this.snake.render(this.map);
+        this.snake.move(this.map);
+        this.snake.render(this.map);
+        // 2.开始游戏的逻辑
+    }
+    // 暴露构造函数给外部
+    window.Game = Game;
+})();
+// // 测试
+var map = document.getElementById('map');
+var game =new Game(map);
+game.start(map);
---
+++ Web/JS高级/贪吃蛇分解/04-贪吃蛇-move方法/js/snake.js
@@ -0,0 +1,77 @@
+// 自调用函数，开启一个新的局部作用域，防止命名冲突
+(function () {
+    var position = 'absolute';
+    // 记录之前创建的蛇
+    var elements = [];
+    function Snake(options) {
+        options = options || {};
+        // 蛇节的大小
+        this.width = options.width || 20;
+        this.height = options.height || 20;
+        // 蛇移动的方向
+        this.direction = options.direction || 'right';
+        // 蛇的身体 第一个元素是蛇头
+        this.body = [
+            {x: 3,y: 2, color: 'red'},
+            {x: 2,y: 2, color: 'blue'},
+            {x: 1,y: 2, color: 'blue'}
+        ];
+    }
+    Snake.prototype.render = function (map) {
+        // 删除之前创建的蛇
+        remove();
+        // 把每一个蛇节渲染到地图上
+        for (var i = 0 ,len = this.body.length; i < len; i++) {
+            // 蛇节
+            var object = this.body[i];
+            var div = document.createElement('div');
+            map.appendChild(div);
+            // 记录当前的蛇
+            elements.push(div);
+            // 设置样式
+            div.style.width = this.width + 'px';
+            div.style.position = position;
+            div.style.height = this.height + 'px';
+            div.style.left = object.x * this.width + 'px';
+            div.style.top = object.y * this.height + 'px';
+            div.style.backgroundColor = object.color;
+        }
+    }
+    // 私有的成员
+    function remove() {
+        for (var i = elements.length - 1; i >= 0; i --) {
+            // 删除div
+            elements[i].parentNode.removeChild(elements[i]);
+            // 删除数组中的元素
+            elements.splice(i, 1);
+        }
+    }
+
+    // 控制蛇移动的方法
+    Snake.prototype.move = function () {
+        // 控制蛇的身体移动（当前蛇节到上一个蛇节的位置）
+        for (var i = this.body.length - 1; i > 0; i--) {
+            this.body[i].x = this.body[i - 1].x;
+            this.body[i].y = this.body[i - 1].y;
+        }
+        // 控制蛇头的移动
+        // 判断蛇移动的方向
+        var head = this.body[0];
+        switch(this.direction) {
+            case 'right':
+                head.x += 1;
+                break;
+            case 'left':
+                head.x -= 1;
+                break;
+            case 'top':
+                head.y -= 1;
+                break;
+            case 'bottom':
+                head.y += 1;
+                break;
+        }
+    }
+    // 暴露构造函数给外部
+    window.Snake = Snake;
+})()
