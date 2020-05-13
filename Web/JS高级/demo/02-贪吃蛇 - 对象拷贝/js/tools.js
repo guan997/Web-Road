2a54aa9b37ec9a20f2e11f57404d03be1147bf24
ggz_66@163.com <ggz_66@163.com>
2020-05-13 10:01:13 +0800

    拷贝

---
+++ Web/JS高级/demo/02-贪吃蛇 - 对象拷贝/js/tools.js
@@ -0,0 +1,19 @@
+// 1.减少作用于查找范围
+// 2.在代码压缩的时候可以减少文件体积
+// 3.声明依赖
+;
+(function () {
+    var Tools = {
+        getRandom: function (min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+    }
+    // 将内部的util提升到window，外部就可以访问到了
+    window.Tools = Tools;
+})()
+// 或者
+// var Tools = {
+//     getRandom: function (min, max) {
+//       return Math.floor(Math.random() * (max - min + 1)) +  min;
+//     }
+//   }
