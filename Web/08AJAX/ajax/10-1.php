642a9b5dd615faaf94eba0f719e48ac8044a9d85
ggz_66@163.com <ggz_66@163.com>
2020-06-20 21:30:12 +0800

    ajax

---
+++ Web/08AJAX/ajax/10-1.php
@@ -0,0 +1,5 @@
+<?php 
+    $uname = $_GET['username'];
+    $pw = $_GET['password'];
+    echo $uname.'------'.$pw;
+ ?>
---
+++ Web/08AJAX/ajax/10-2.php
@@ -0,0 +1,6 @@
+<?php 
+    $uname = $_POST['username'];
+    $pw = $_POST['password'];
+    // echo $uname.'------'.$pw;
+    echo '{"username":"'.$uname.'","password":"'.$pw.'"}';
+ ?>
