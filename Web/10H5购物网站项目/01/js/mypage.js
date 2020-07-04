516ab1ad5fe3f1dd1d312415f39bfcc22819b680
ggz_66@163.com <ggz_66@163.com>
2020-07-05 00:45:39 +0800

    第一屏布局和动画

--- Web/10H5购物网站项目/01/js/mypage.js
+++ Web/10H5购物网站项目/01/js/mypage.js
@@ -1,5 +1,8 @@
 $(function() {
 	$('#fullpage').fullpage({// fullpage()方法接受的是json对象
         navigation: true,// 是否显示项目导航
+        // navigationPosition: 'left', // 项目导航的位置，可选 left 或 right
+        // loopBottom: true, // 滚动到最底部后是否滚回顶部
+        // scrollingSpeed: 1300// 滚动速度，单位为毫秒
     }); 
 });
