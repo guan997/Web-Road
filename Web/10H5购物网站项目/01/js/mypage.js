51ffb7cb21ef3bc382324727d2dc0f7b7cf796a6
ggz_66@163.com <ggz_66@163.com>
2020-07-07 11:57:53 +0800

    第七屏幕布局和动画

--- Web/10H5购物网站项目/01/js/mypage.js
+++ Web/10H5购物网站项目/01/js/mypage.js
@@ -1,28 +1,36 @@
 $(function () {
     var k = $(window).height();// 当前屏幕的高度
     var flag = false;//动画是否执行完毕
+    $(".next").click(function(event) {
+        // 点击next往下播放一个屏幕
+        $.fn.fullpage.moveSectionDown();
+    })
     $('#fullpage').fullpage({// fullpage()方法接受的是json对象
         navigation: true,// 是否显示项目导航
         // navigationPosition: 'left', // 项目导航的位置，可选 left 或 right
         // loopBottom: true, // 滚动到最底部后是否滚回顶部
+
         scrollingSpeed: 1200, // 滚动速度，单位为毫秒
+
         // 滚动到第二屏后的回调函数，接收 anchorLink 和 index 两个参数，
         // anchorLink 是锚链接的名称，index 是序号，从1开始计算
         afterLoad: function (anchorLink, index) {
-            if (index == 2 && flag == false) {
+            if (index == 2) {
+                // 往第2屏幕滚动时next先消失，等所有动画显示，next淡出
+                $(".next").fadeOut();
                 // 缓动动画要加到时间的后面，函数的前面
                 $(".search").show().animate({ "right": 370 }, 1500, "easeOutBack", function () {
                     // 方块走进来，沙发2个字显示
                     $(".search-words").animate({ 'opacity': 1 }, 500, function () {
                         $(".search").hide();
                         // 图片往右上角缩小
-                        $(".search-02-1").show().animate({ "height": 30, "right": 250, "bottom": 452 }, 1000, function () {
-                            flag = true; // flag为true说明所有动画执行完毕
-                        });
+                        $(".search-02-1").show().animate({ "height": 30, "right": 250, "bottom": 452 }, 1000);
                         // 同时沙发图片变大
                         $(".goods-02").show().animate({ "height": 218 }, 1000);
                         // 同时白色文字渐渐显示出来
-                        $(".words-02").animate({ "opacity": 1 }, 500);
+                        $(".words-02").animate({ "opacity": 1 }, 500, function() {
+                            $(".next").fadeIn();
+                        });
                     });
                 })
             }
@@ -33,11 +41,14 @@ $(function () {
         // nextIndex 是滚动到的“页面”的序号，从1开始计算；
         // direction 判断往上滚动还是往下滚动，值是 up 或 down。
         onLeave: function (index, nextIndex, direction) {
-            if (index == 2 && nextIndex == 3 && flag == true) {
+            $(".next").fadeOut();
+            if (index == 2 && nextIndex == 3) {
                 // 第二屏幕往第三屏幕滚动的时候，沙发往第三屏幕跑
                 $(".shirt-02").show().animate({ "bottom": -(k - 250), "width": 207, "left": 260 }, 2000, function () {
                     $(".img-01-a").animate({ "opacity": 1 }, 500, function () {
-                        $(".btn-01-a").animate({ "opacity": 1 }, 500);
+                        $(".btn-01-a").animate({ "opacity": 1 }, 500, function() {
+                            $(".next").fadeIn();
+                        });
                     });
                 });
                 $(".cover").show();
@@ -52,7 +63,9 @@ $(function () {
                     // 购物车开始往右走
                     $(".car").animate({ "left": 1500 }, 3000, "easeInElastic", function () {
                         $(".note").show();
-                        $(".note-img, .words-4-a").animate({ "opacity": 1 }, 1000);
+                        $(".note-img, .words-4-a").animate({ "opacity": 1 }, 1000, function() {
+                            $(".next").fadeIn();
+                        });
                     });
                 })
             }
@@ -67,6 +80,7 @@ $(function () {
                         // 单子上来
                         $(".order-05").animate({ "bottom": 390 }, function () {
                             $(".words-05").addClass("words-05-a");
+                            $(".next").fadeIn();
                         });
                     });
                 })
@@ -94,6 +108,7 @@ $(function () {
                                         $(".girl").show().animate({ "right": 350, "height": 305 }, 300, function () {
                                             $(".pop-07").show();
                                             $(".words-06-a").show();
+                                            $(".next").fadeIn();
                                         });
                                     });
                                 });
@@ -109,6 +124,7 @@ $(function () {
                 setTimeout(function () {
                     $(".star").animate({ "width": 120 }, 500, function () {
                         $(".good-07").show();
+                        $(".next").fadeIn();
                     });
                 }, 2000);
             }
@@ -119,16 +135,29 @@ $(function () {
             //     $(".btn-08-a").hide();
             // })
             // hover来替代更简洁
-            $(".benginShoping").hover(function() {
+            $(".benginShoping").hover(function () {
                 $(".btn-08-a").toggle(); //toggle显示和隐藏
             });
             // 大手跟随鼠标移动
-            $(document).mousemove(function(event) {
-                // 把鼠标在页面的坐标给hand大手 left top
+            $(document).mousemove(function (event) {
                 var x = event.pageX - $(".hand-08").width() / 2;//获得鼠标在界面的x坐标
                 var y = event.pageY + 10;//获得鼠标在界面的x坐标
-                $(".hand-08").css({"left": x, "top": y});
+                // 把鼠标在页面的坐标给hand大手 left top
+                var minY = k - 449;
+                // 手的top不能小于（当前屏幕的高度k - 瘦的高度449）
+                if (y < minY) {
+                    y = minY;
+                }
+                $(".hand-08").css({ "left": x, "top": y });
             })
+            // 当点击再来一次的时候，分两步进行
+            $(".again").click(function(ecent) {
+                // 1.返回第1屏幕
+                $.fn.fullpage.moveTo(1);
+                // 2. 所有的动画都复原 (核心原理 让动画的元素清除行内元素)
+                // 所有动画的盒子添加  move清除
+                $("img,.move").attr({"style": ""});
+             })
         }
     });
 });
