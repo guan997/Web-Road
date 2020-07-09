2994bb134d9f50b75acfd70249189f23bfbf53ff
ggz_66@163.com <ggz_66@163.com>
2020-07-09 21:16:01 +0800

    背景旋转效果

--- Web/11H5项目和新增API/QQ浏览器/js/myqq.js
+++ Web/11H5项目和新增API/QQ浏览器/js/myqq.js
@@ -8,5 +8,13 @@ $(function() {
         navigation: true,
         // 循环滚动
         loopBottom: true,
+        onLeave: function(index, nextIndex, direction) {
+            if(nextIndex != 1) {
+                // 如果不是第一屏幕，就给背景添加旋转
+                $("#bg").addClass("rotate");
+            }else {
+                $("#bg").removeClass("rotate");
+            }
+        }
     })
 })
