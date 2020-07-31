0d0e76f787f2285f1e45628dec9cc021ce48e468
ggz_66@163.com <ggz_66@163.com>
2020-07-31 18:09:06 +0800

    添加购物车

--- Web/14Vue商城项目/03/02.vue-cms/src/components/goods/GoodsInfo.vue
+++ Web/14Vue商城项目/03/02.vue-cms/src/components/goods/GoodsInfo.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="goodsinfo-container">
+    <!-- 小球动画 -->
+    <transition @enter="enter" @before-enter="beforeEnter" @after-enter="afterEnter">
+      <div class="ball" v-show="ballFlag" ref="ball"></div>
+    </transition>
     <!-- 这是轮播图区域 -->
     <div class="mui-card">
       <div class="mui-card-content">
@@ -18,11 +22,14 @@
             销售价格：
             <span class="now_price">￥{{ goodsinfo.sell_price}}</span>
           </p>
-          <p class="price">购买数量：<number></number></p>
+          <p class="price">
+            购买数量：
+            <number :getcount="getSelectedCount" :max="goodsinfo.stock_quantity"></number>
+          </p>
           <p>
-            <mt-button type="primary" size="small" @click="getGoodsInfo">立即购买</mt-button>
-            <mt-button type="danger" size="small" @click="getGoodsInfo">加入购物车</mt-button>
-             <!-- 分析： 如何实现加入购物车时候，拿到 选择的数量 -->
+            <mt-button type="primary" size="small">立即购买</mt-button>
+            <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
+            <!-- 分析： 如何实现加入购物车时候，拿到 选择的数量 -->
             <!-- 1. 经过分析发现： 按钮属于 goodsinfo 页面， 数字 属于 numberbox 组件 -->
             <!-- 2. 由于涉及到了父子组件的嵌套了，所以，无法直接在 goodsinfo 页面zhong 中获取到 选中的商品数量值-->
             <!-- 3. 怎么解决这个问题：涉及到了 子组件向父组件传值了（事件调用机制） -->
@@ -60,6 +67,7 @@ export default {
       selectedCount: 1, // // 保存用户选中的商品数量， 默认，认为用户买1个
       goodsinfo: [], //存放商品的信息
       lunbotuLists: [], // 轮播图的数据
+      ballFlag: false, // 控制小球的隐藏和显示的标识符
     };
   },
   created() {
@@ -69,13 +77,11 @@ export default {
   methods: {
     // 获取商品列表
     getGoodsInfo() {
-      this.$http
-        .get("api/goods/getinfo/" + this.id)
-        .then((result) => {
-          if (result.body.status === 0) {
-            this.goodsinfo = result.body.message[0];
-          }
-        });
+      this.$http.get("api/goods/getinfo/" + this.id).then((result) => {
+        if (result.body.status === 0) {
+          this.goodsinfo = result.body.message[0];
+        }
+      });
     },
     // 获取轮播图的方法
     getLunbotus() {
@@ -90,18 +96,39 @@ export default {
         }
       });
     },
-    getDesc(id){
-        // 点击使用编程式导航跳转到图文介绍页面
-        this.$router.push({name:'goodsdesc',params:{id}});
+    getDesc(id) {
+      // 点击使用编程式导航跳转到图文介绍页面
+      this.$router.push({ name: "goodsdesc", params: { id } });
     },
-    getComment(id){
-        // 点击跳转到评论介绍页面
-        this.$router.push({name:'goodscomment',params:{id}});
+    getComment(id) {
+      // 点击跳转到评论介绍页面
+      this.$router.push({ name: "goodscomment", params: { id } });
+    },
+    addToShopCar() {
+      // 添加到购物车
+      this.ballFlag = !this.ballFlag;
+    },
+    getSelectedCount(count){
+      // 当子组件把 选中的数量传递给父组件的时候，把选中的值保存到 data 上
+      this.selectedCount = count;
+      console.log("父组件拿到的数量值是："+ this.selectedCount);
+    },
+    beforeEnter(el) {
+      el.style.transform = "translate(0,0)";
+    },
+    enter(el, done) {
+      el.offsetWidth;
+      el.style.transform ="translate(93px, 230px)";
+      el.style.transition = "all 0.5s cubic-bezier(.4,-0.3,1,.68)";
+      done();
+    },
+    afterEnter(el){
+      this.ballFlag = !this.ballFlag;
     }
   },
   components: {
     "swiper-box": swiper,
-    "number":number
+    "number": number,
   },
 };
 </script>
@@ -119,6 +146,16 @@ export default {
     button {
       margin: 15px 0;
     }
+  }
+  .ball {
+    width: 15px;
+    background: red;
+    position: absolute;
+    border-radius: 50%;
+    height: 15px;
+    z-index: 99;
+    top: 390px;
+    left: 146px;
   }
 }
 </style>
