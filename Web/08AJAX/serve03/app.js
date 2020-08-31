2f2f8c8fda1eee84e1f25d018691024f37e17c53
ggz_66@163.com <ggz_66@163.com>
2020-08-31 18:31:00 +0800

    实现跨域登录功能

--- Web/08AJAX/serve03/app.js
+++ Web/08AJAX/serve03/app.js
@@ -2,11 +2,20 @@
 const express = require('express');
 // 路径处理模块
 const path = require('path');
-const formidable = require('formidable');
+const request = require('request');
 // 创建web服务器
 const app = express();
 // 静态资源访问服务功能
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/serve', (req, res) => {
+	request('http://localhost:3001/cross', (err, response, body) => {
+		// console.log(err)
+		// console.log(response)
+		// console.log(body)
+		res.send(body)
+	})
+})
 
 // 监听端口
 app.listen(3000);
