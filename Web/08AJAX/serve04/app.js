1d7cd2190d014bb45b5883c244fe97b535eaf997
ggz_66@163.com <ggz_66@163.com>
2020-09-03 15:58:06 +0800

    任务管理列表

--- Web/08AJAX/serve04/app.js
+++ Web/08AJAX/serve04/app.js
@@ -2,10 +2,18 @@
 const express = require('express');
 // 路径处理模块
 const path = require('path');
+// 导入mongoose模块
+const mongoose = require('mongoose');
 // 创建web服务器
 const app = express();
 // 静态资源访问服务功能
 app.use(express.static(path.join(__dirname, 'public')));
+// 导入bodyParser模块
+const bodyParser = require('body-parser');
+// 处理post请求参数
+app.use(bodyParser.json()); 
+// 数据库连接
+mongoose.connect('mongodb://tododata:tododata@localhost:27017/todo', {useNewUrlParser: true })
 
 app.get('/base', (req, res) => {
     res.send({
@@ -25,6 +33,49 @@ app.get('/user', (req, res) => {
 app.post('/user', (req, res) => {
     res.send(req.query)
 })
+
+app.get('/jsonp', (req, res) => {
+    const cb = req.query.cb
+    const data = cb + "{{name: 'zhaoli}}"
+    res.send(data)
+})
+
+// 导入todo路由案例
+const todoRouter = require('./route/todo.js')
+// 当客户端的请求路径以/todo开头时
+app.use('/todo', todoRouter);
+
+// 获取用户列表信息
+app.get('/users', (req, res) => {
+	res.send('当前是获取用户列表信息的路由');
+});
+
+// 获取某一个用户具体信息的路由
+app.get('/users/:id', (req, res) => {
+	// 获取客户端传递过来的用户id
+	const id = req.params.id;
+	res.send(`当前我们是在获取id为${id}用户信息`);
+});
+
+// 删除某一个用户
+app.delete('/users/:id', (req, res) => {
+	// 获取客户端传递过来的用户id
+	const id = req.params.id;
+	res.send(`当前我们是在删除id为${id}用户信息`);
+});
+
+// 修改某一个用户的信息
+app.put('/users/:id', (req, res) => {
+	// 获取客户端传递过来的用户id
+	const id = req.params.id;
+	res.send(`当前我们是在修改id为${id}用户信息`);
+});
+
+app.get('/xml', (req, res) => {
+	res.header('content-type', 'text/xml');
+	res.send('<message><title>消息标题</title><content>消息内容</content></message>')
+});
+
 
 // 监听端口
 app.listen(3000);
