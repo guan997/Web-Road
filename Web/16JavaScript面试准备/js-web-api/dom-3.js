4b2ec832b52b2ebc9402588e6a78d536f4122685
ggz_66@163.com <ggz_66@163.com>
2020-09-14 00:04:36 +0800

    缓存DOM查询结果

--- Web/16JavaScript面试准备/js-web-api/dom-3.js
+++ Web/16JavaScript面试准备/js-web-api/dom-3.js
@@ -1,10 +1,25 @@
+// DOM查询做缓存
+
+// 不缓存DOM查询结果
+// for(let i = 0; i < document.getElementsByTagName('p').length; i++){
+//     // 每次循环，都会计算length，频繁进行DOM计算
+// }
+
+// // 缓存DOM查询结果
+// const pList = document.getElementsByTagName('p')
+// const length = pList.length
+// for(let i = 0; i < length; i++){
+//     // 缓存length，只进行一次Dom查询
+// }
+
+
 // 将频繁操作改为一次性操作
 const list = document.getElementById('list')
 
 // 创建一个文档片段，此时还没有插入到DOM结构中
 const frag = document.createDocumentFragment()
 
-for(let i = 0; i <10; i++){
+for (let i = 0; i < 10; i++) {
     const li = document.createElement('li')
     li.innerHTML = 'List item' + i
     frag.appendChild(li)
